e51968d58b4b473eb82d06e4ad1e9c46d2a1cf81
cluster <yanl.fu@outlook.com>
2021-09-03 19:23:30 +0800

    提交comservice初始化JsonAdaptor类

--- platform-common/platform-common-comservice/src/main/java/com/platform/comservice/PlatformComServiceApplication.java
+++ platform-common/platform-common-comservice/src/main/java/com/platform/comservice/PlatformComServiceApplication.java
@@ -1,5 +1,6 @@
 package com.platform.comservice;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author: fuyl
  * @create: 2020-09-10 17:03
  **/
-@SpringBootApplication(scanBasePackages = {"com.platform","com.lemon"})
+@SpringBootApplication(scanBasePackages = {"com.platform","com.lemon","com.platform.common"})
 public class PlatformComServiceApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformComServiceApplication.class);
+    }
 }
